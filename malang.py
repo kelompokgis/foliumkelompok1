cdb73c6dea609a2b2089edcf599bbeaac440fe33
shintaamelia30 <Shinta.amelia216@gmail.com>
2017-10-28 22:07:16 +0700

    Update malang.py

--- malang.py
+++ malang.py
@@ -1,1 +1,14 @@
 import folium
+
+m = folium.map(
+  location =[-7.9786395,112.5617421]'
+  zoom_start = 12,
+)
+
+folium.Marker(
+  location =[-7.976854,112.6307393],
+  popup='Kahuripan',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)
+
+m.save('index.html')
