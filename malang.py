e627cf608b19a514c64a37d3f5680d570dfa9942
ekapratamap <ekapratamaputra05@gmail.com>
2017-10-28 09:15:57 -0700

    Update malang.py

--- malang.py
+++ malang.py
@@ -63,6 +63,66 @@ folium.Marker(
   location =[-7.968588,112.6340983],
   popup='Dokter Wahidin',
   icon = folium.Icon (icon='info-sign')
+).add_to(m)
+         
+  folium.Marker(
+  location =[-7.964214,112.6351963],
+  popup='Khayangan Wayang',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)
+  
+  folium.Marker(
+  location =[-7.963571,112.6321957],
+  popup='Kaliurang',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)      
+         
+  folium.Marker(
+  location =[-7.963757,112.6311337],
+  popup='Theretes Selatan',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)
+         
+  folium.Marker(
+  location =[-7.963417,112.6290577],
+  popup='Ngantang 1',
+  icon = folium.Icon (icon='info-sign')
 ).add_to(m) 
-
+     
+   folium.Marker(
+  location =[-7.963173,112.6294597],
+  popup='Ngantang 3',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)        
+         
+   folium.Marker(
+  location =[-7.964066,112.6285907],
+  popup='Sukapura',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)       
+         
+   folium.Marker(
+  location =[-7.962982,112.6274317],
+  popup='Tapak Siring',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)       
+         
+   folium.Marker(
+  location =[-7.961415,112.6318417],
+  popup='Nongko Jajar 1',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)   
+         
+   folium.Marker(
+  location =[-7.960716,112.6326168],
+  popup='Nongko Jajar 3',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)         
+         
+   folium.Marker(
+  location =[-7.960729,112.6321123],
+  popup='Sarangan',
+  icon = folium.Icon (icon='info-sign')
+).add_to(m)         
+        
 m.save('index.html')
